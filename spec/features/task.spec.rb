43464798f486d05ad2984a9cdb6497ab57f5a9bb
Blaise-gustave <uyisengablaise@gmail.com>
2019-10-02 13:53:18 +0000

    step4 to 8

--- spec/features/task.spec.rb
+++ spec/features/task.spec.rb
@@ -1,14 +1,14 @@
 # With this require, Capybara and other features required for Feature Spec are enabled.
 require 'rails_helper'
 
-# On the right side of this RSpec.feature, write the test item name like "task management feature" (grouped by do ~ end)
+# On the right side of this RSpec.feature,  write the test item name like "task management feature" (grouped by do ~ end)
 RSpec.feature "Task management function", type: :feature do
-  # In scenario (alias of it), write the processing of the test for each item you want to check.
+  # In scenario (alias of it), write   the processing of the test for each item you want to check.
   scenario "Test task list" do
     Task.create!(name: 'adali', details: 'football')
     Task.create!(name: 'patrick', details: 'bicycle')
   
-    # visit to tasks_path (transition to task list page)
+    # visit to tasks_path (transition to  task list page)
     visit tasks_path
   
     # write a test to verify that the string "testtesttest"  "samplesample " is included when accessing the task list page using have_content method
